b22876841b146ee8092348f5cd8c2559490aa8cc
Agostina Avalle <avalleag@gmail.com>
2026-05-12 23:53:04 -0300

    add initial docker configuration

---
+++ src/main/java/com/signasource/signa_api/SignaApiApplication.java
@@ -0,0 +1,13 @@
+package com.signasource.signa_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/signasource/signa_api/SignaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.signasource.signa_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
