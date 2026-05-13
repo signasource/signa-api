6b54baf4098a20b27cf11590a87630e301df4fcf
Agostina Avalle <avalleag@gmail.com>
2026-05-13 03:28:51 -0300

    add

--- .release.config.mjs
+++ .release.config.mjs
@@ -1,65 +1,10 @@
 export default {
-    branches: ['master'],
+  branches: ["main"],
 
-    plugins: [
-        [
-            '@semantic-release/commit-analyzer',
-            {
-                preset: 'conventionalcommits',
-                releaseRules: [
-                    { type: 'feat', release: 'minor' },
-                    { type: 'fix', release: 'patch' },
-                    { type: 'perf', release: 'patch' },
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
 
-                    { type: 'docs', release: false },
-                    { type: 'chore', release: false },
-                    { type: 'refactor', release: false },
-                    { type: 'test', release: false },
-                    { type: 'ci', release: false },
-                    { type: 'build', release: false },
-                ],
-            },
-        ],
-
-        [
-            '@semantic-release/release-notes-generator',
-            {
-                preset: 'conventionalcommits',
-                presetConfig: {
-                    types: [
-                        {
-                            type: 'feat',
-                            section: '✨ Features',
-                        },
-                        {
-                            type: 'fix',
-                            section: '🐛 Fixes',
-                        },
-                        {
-                            type: 'perf',
-                            section: '⚡ Performance',
-                        },
-                    ],
-                },
-            },
-        ],
-
-        [
-            '@semantic-release/changelog',
-            {
-                changelogFile: 'CHANGELOG.md',
-            },
-        ],
-
-        '@semantic-release/github',
-
-        [
-            '@semantic-release/git',
-            {
-                assets: ['CHANGELOG.md'],
-                message:
-                    'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-            },
-        ],
-    ],
+    "@semantic-release/github"
+  ]
 };
