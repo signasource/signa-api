132a9d9d73692b6b5b7c4ca51f72b7b23c13c185
Agostina Avalle <avalleag@gmail.com>
2026-06-10 17:31:53 -0300

    send email async

--- src/main/java/com/signasource/signa_api/auth/service/EmailService.java
+++ src/main/java/com/signasource/signa_api/auth/service/EmailService.java
@@ -3,6 +3,7 @@ package com.signasource.signa_api.auth.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import jakarta.mail.internet.MimeMessage;
@@ -24,6 +25,7 @@ public class EmailService {
 	@Value("${spring.mail.username}")
 	private String fromEmail;
 
+	@Async
 	public void sendVerificationEmail(String to, String token) {
 		sendEmail(to, "Verify your account", buildVerificationHtml(buildUrl(VERIFICATION_PATH, token)));
 	}
