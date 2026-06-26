1560963f90f20f1e7fe390df36c3833a945ac373
Agostina Avalle <91240071+agosav@users.noreply.github.com>
2026-06-26 19:45:26 -0300

    feat(notifications): agrega conexión con firebase

--- src/test/java/com/signasource/signa_api/SignaApiApplicationTests.java
+++ src/test/java/com/signasource/signa_api/SignaApiApplicationTests.java
@@ -3,10 +3,17 @@ package com.signasource.signa_api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import com.google.firebase.messaging.FirebaseMessaging;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class SignaApiApplicationTests {
+
+	@MockitoBean
+	private FirebaseMessaging firebaseMessaging;
+
 	@Test
 	void contextLoads() {
 	}
