395d4edc410441d37361cf45963986c35812338b
Juan Cruz Lopez Freytas <juancruzlopezfreytas@gmail.com>
2026-07-08 18:48:42 -0300

    delete a decorator

--- src/main/java/com/signasource/signa_api/users/entity/User.java
+++ src/main/java/com/signasource/signa_api/users/entity/User.java
@@ -29,7 +29,6 @@ public class User {
 	@Column(nullable = false, unique = true)
 	private String email;
 
-	@Column(name = "password_hash")
 	private String passwordHash;
 
 	@Column(nullable = false)
