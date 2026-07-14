13954e4ff805b43a5375e91b2d1370fa7d3ef750
Juan Cruz Lopez Freytas <juancruzlopezfreytas@gmail.com>
2026-07-14 03:34:14 -0300

    fix wildcard imports

--- src/test/java/com/signasource/signa_api/users/service/FriendshipServiceTest.java
+++ src/test/java/com/signasource/signa_api/users/service/FriendshipServiceTest.java
@@ -1,6 +1,7 @@
 package com.signasource.signa_api.users.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
