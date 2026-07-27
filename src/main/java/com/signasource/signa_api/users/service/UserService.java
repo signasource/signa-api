6ddbc974d8942310c62269c43a78c591294f8ef7
mate123456789 <ottonello.mateo@gmail.com>
2026-07-27 18:32:42 -0300

    solo se pueden ver las cuentas que están enabled = true

--- src/main/java/com/signasource/signa_api/users/service/UserService.java
+++ src/main/java/com/signasource/signa_api/users/service/UserService.java
@@ -58,6 +58,9 @@ public class UserService {
     }
 
     private boolean canView(User target, User requester) {
+        if (!target.isEnabled()) {
+            return false;
+        }
         if (target.getAccountVisibility() == AccountVisibility.PUBLIC) {
             return true;
         }
